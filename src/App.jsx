bde8481578988de9a695c496e7501dbee86f764b
Gaurang Jodh <jodhgaurang@gmail.com>
2025-04-06 14:44:36 +0530

    Add the Gemini API and update the chat history

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ChatbotIcon from "./components/ChatbotIcon";
 import ChatForm from "./components/ChatForm";
 import ChatMessage from "./components/ChatMessage";
 
 const App = () => {
   const [chatHistory, setChatHistory] = useState([]);
+  const chatBodyRef = useRef();
 
-  const generateBotResponse = (history) => {
-    console.log(history);
-  }
+  const generateBotResponse = async (history) => {
+
+    // Helper function to update chat's history
+    const updateHistory = (text) => {
+      setChatHistory(prev => [...prev.filter(msg => msg.text !== "Soch raha hu...."), {role:"model", text}]);
+    }
+    
+    // Format chat history for API request
+    history = history.map(({role,text}) => ({role, parts: [{text}]}));
+
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({ contents: history})
+    }
+
+    try {
+      //Make the API call to get the bots response
+      const response = await fetch(import.meta.env.VITE_API_URL, requestOptions);
+      const data = await response.json();
+      if(!response.ok) throw new Error(data.error.message || "Kuch toh gadbad hai!");
+
+      // Clean and update chat history with bots response
+      const apiResponseText = data.candidates[0].content.parts[0].text.replace(/\*\*(.*?)\*\*/g, "$1").trim();
+      updateHistory(apiResponseText);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    // Auto scroll whenever chat history updates
+    chatBodyRef.current.scrollTo({top: chatBodyRef.current.scrollHeight, behavior: "smooth"});
+  }, [chatHistory]);
 
   return ( 
   <div className="container">
+    <button id="chatbot-toggler">
+      <span className="material-symbols-rounded">mode_comment</span>
+      <span className="material-symbols-rounded">close</span>
+    </button>
     <div className="chatbot-popup">
       {/* Chatbot Header */}
       <div className="chat-header">
@@ -24,7 +60,7 @@ keyboard_arrow_down
 </button>
       </div>
       {/* Chatbot Body */}
-      <div className="chat-body">
+      <div ref={chatBodyRef} className="chat-body">
         <div className="message bot-message">
           <ChatbotIcon />
           <p className="message-text">
